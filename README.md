dbe88eebfd3a722fa774b2ce1f16d01efccf370f
Unustudents <nandaakmal40@gmail.com>
2025-02-16 21:51:28 +0700

    REFAKTORING - Update project name and description in pubspec.yaml; revise Flutter and Dart versions in README.md; enhance password update logic in GantiPasswordCubit with improved error handling and formatting; add necessary permissions in README.md for Android

--- README.md
+++ README.md
@@ -8,13 +8,39 @@ Generasi baru berkonsultasi
 This project is licensed under the Apache License 2.0. See the [LICENSE](./LICENSE) file for details.
 
 ## Version Flutter
-- Flutter version 3.24.3 on channel stable
-- Dart version 3.5.3
-- DevTools version 2.37.3
+- Flutter version 3.27.1 on channel stable
+- Dart version 3.6.0
+- DevTools version 2.40.2
 
-tambahkan 
-<uses-permission android:name="android.permission.INTERNET" />
+## tambahkan
+Tambahkan ke android/app/src/main/Androidmanifest.xml di dalam tag <manifest ...>
+```xml
+<!-- Internet permissions do not affect the `permission_handler` plugin, but are required if your app needs access to the internet. -->
+<uses-permission android:name="android.permission.INTERNET"/>
+<!-- Permissions options for the `storage` group -->
 <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
+<!-- Read storage permission for Android 12 and lower -->
 <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
+<!-- Granular media permissions for Android 13 and newer.
+See https://developer.android.com/about/versions/13/behavior-changes-13#granular-media-permissions for more information. -->
+<uses-permission android:name="android.permission.READ_MEDIA_IMAGES" />
+<!-- Permissions options for the `manage external storage` group -->
+<uses-permission android:name="android.permission.MANAGE_EXTERNAL_STORAGE" />
+
+```
+```gradle
+# ubah com.android.application di andorid/settings.gradle
+{
+    id "com.android.application" version "8.2.1"
+}
+
+# ubah compileSdk (minimal 34) di android/app/build.gradle
+android{
+    ...
+    compileSdk=34
+}
+
 android:requestLegacyExternalStorage="true"
-com.android.application = 8.2.1
+tambahkan [minifyEnabled true, proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'] di android/app/build.gradle pada bagian buildTypes/release
+tambahkan [org.gradle.parallel=true, org.gradle.caching=true] di android/gradle.properties
+```
--- lib/features/profile/presentation/cubit/ganti_password_cubit.dart
+++ lib/features/profile/presentation/cubit/ganti_password_cubit.dart
@@ -2,39 +2,59 @@ import 'dart:convert';
 
 import 'package:bloc/bloc.dart';
 import 'package:crypto/crypto.dart';
-import 'package:ekonseling/supabase_config.dart';
 import 'package:equatable/equatable.dart';
+
+import '../../../../supabase_config.dart';
+
 export 'package:flutter_bloc/flutter_bloc.dart';
+
 part 'ganti_password_state.dart';
 
 class GantiPasswordCubit extends Cubit<GantiPasswordState> {
   GantiPasswordCubit() : super(GantiPasswordState());
 
   // FUNCTION - UNTUK MEMPERBARUI KATA SANDI
-  Future<void> onUpdatePassword({required String currentPassword, required String newPassword}) async {
+  Future<void> onUpdatePassword(
+      {required String currentPassword, required String newPassword}) async {
     emit(state.copyWith(status: GantiPasswordStatus.loading));
     String hashCurrent = hashedPassword(currentPassword);
     String hashNew = hashedPassword(newPassword);
     try {
-      final PostgrestMap? passHash = await SupabaseConfig.client.from('users').select('pass_hash').eq('id', SupabaseConfig.client.auth.currentUser!.id).maybeSingle();
+      final PostgrestMap? passHash = await SupabaseConfig.client
+          .from('users')
+          .select('pass_hash')
+          .eq('id', SupabaseConfig.client.auth.currentUser!.id)
+          .maybeSingle();
       if (passHash == null || passHash.isEmpty) {
-        emit(state.copyWith(status: GantiPasswordStatus.failure, msg: "Error ketika memeriksa password"));
+        emit(state.copyWith(
+            status: GantiPasswordStatus.failure,
+            msg: "Error ketika memeriksa password"));
         return;
       }
       if (passHash.isNotEmpty) {
         final bool response = passHash.containsValue(hashCurrent);
         if (!response) {
-          emit(state.copyWith(status: GantiPasswordStatus.failure, msg: "Password lama tidak sesuai"));
+          emit(state.copyWith(
+              status: GantiPasswordStatus.failure,
+              msg: "Password lama tidak sesuai"));
           return;
         }
         if (response) {
-          await SupabaseConfig.client.auth.updateUser(UserAttributes(password: hashNew));
-          await SupabaseConfig.client.from('users').update({'pass_hash': hashNew}).eq('id', SupabaseConfig.client.auth.currentUser!.id);
-          emit(state.copyWith(status: GantiPasswordStatus.success, msg: "Password berhasil diubah"));
+          await SupabaseConfig.client.auth
+              .updateUser(UserAttributes(password: hashNew));
+          await SupabaseConfig.client
+              .from('users')
+              .update({'pass_hash': hashNew}).eq(
+                  'id', SupabaseConfig.client.auth.currentUser!.id);
+          emit(state.copyWith(
+              status: GantiPasswordStatus.success,
+              msg: "Password berhasil diubah"));
         }
       }
     } catch (e) {
-      emit(state.copyWith(status: GantiPasswordStatus.failure, msg: "Error ketika mengubah password"));
+      emit(state.copyWith(
+          status: GantiPasswordStatus.failure,
+          msg: "Error ketika mengubah password"));
     }
   }
 
